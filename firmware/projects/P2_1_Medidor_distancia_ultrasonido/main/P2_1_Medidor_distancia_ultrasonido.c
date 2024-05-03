e917d43acecc220d9ebaa621f11601c8017f9825
IrinaSacks <sacksduranirina@gmail.com>
2024-05-03 20:36:27 -0300

    Agrega documentacion al ejercicio 1 del proyecto 2

--- firmware/projects/P2_1_Medidor_distancia_ultrasonido/main/P2_1_Medidor_distancia_ultrasonido.c
+++ firmware/projects/P2_1_Medidor_distancia_ultrasonido/main/P2_1_Medidor_distancia_ultrasonido.c
@@ -1,16 +1,16 @@
-/*! @mainpage Template
+/*! @mainpage Medidor de distancia por ultrasonido y mostrado por display
  *
  * @section genDesc General Description
  *
- * This section describes how the program works.
- *
- * <a href="https://drive.google.com/...">Operation Example</a>
+ * Este programa, mediante Tareas, controla si se apreto SWITCH 1 o 2, mide la distancia en centimetros utilizando un sensor de ultrasonido y la muestra por un display. 
+ * Además permite, mediante interrupciones, detener o encencer las mediciones al apretar SWITCH_1 o pausar la actualizacion del display al apretar SWITCH_2 
  *
  * @section hardConn Hardware Connection
  *
  * |    Peripheral  |   ESP32   	|
  * |:--------------:|:--------------|
- * | 	PIN_X	 	| 	GPIO_X		|
+ * | 	ECHO	 	| 	GPIO_3		|
+ * | 	TRIGGER	 	| 	GPIO_2		|
  *
  *
  * @section changelog Changelog
@@ -34,14 +34,33 @@
 #include "lcditse0803.h"
 
 /*==================[macros and definitions]=================================*/
+/** 
+	@brief refresco periodo de 1 segundo para el Delay
+*/
 #define refresco 1000
+/**
+	@brief refresco_tecla periodo de 100 mseg para el Delay usado para leer si se apreto un switch
+*/
 #define refresco_tecla 100
 /*==================[internal data definition]===============================*/
+/*! 
+	@brief distancia Almacena el valor de la distancia medida por el sensor de ultrasonido en cm
+*/
 uint16_t distancia;
+/**
+	@brief control variable que indica True cuando hay que realizar mediciones y mostrarlas por display de lo contrario es falsa
+*/
 bool control=true;
+/*! 
+	@brief pausa variable que indica True si los datos mostrados por display no deben actualizarse
+*/
 bool pausa=false;
 /*==================[internal functions declaration]=========================*/
-void voometro(uint16_t dis_cm){
+/** @brief Prende o apaga determinados leds segun el valor de la distancia medida en cm a la que esta un objeto
+ * @param dis_cm distancia en cm medida por sensor de ultrasonido
+ * @return void
+*/
+void vumetro(uint16_t dis_cm){
 	if(dis_cm < 10){
 		LedOff(LED_1);
 		LedOff(LED_2);
@@ -64,6 +83,10 @@ void voometro(uint16_t dis_cm){
 	}
 }
 
+/** @brief Lee y almacena la distancia cada 1 segundo 
+ * @param *pvParameter
+ * @return static void
+*/
 static void LeerDistancia(void *pvParameter){
     while(1){
 		if(control){
@@ -73,6 +96,10 @@ static void LeerDistancia(void *pvParameter){
     }
 }
 
+/** @brief lee el switch apretado cada 0.1 segundo y segun esto modifica variables booleanas
+ * @param *pvParameter 
+ * @return static void
+*/
 static void LeerTecla(void *pvParameter){
     uint8_t teclas;
 	while(1){
@@ -89,19 +116,21 @@ static void LeerTecla(void *pvParameter){
 	}
 }
 
+/** @brief Actualiza cada 1 segundo la distancia en el display si no se apreto el switch 2, y tambien actualiza el vumetro 
+ * @param *pvParameter 
+ * @return static void
+*/
 static void MostrarDistancia(void *pvParameter){
 	while(1){
 		if(control){
 			if(!pausa){
 				LcdItsE0803Write(distancia);
 			}
-			voometro(distancia);
+			vumetro(distancia);
 		}
 		vTaskDelay(refresco/portTICK_PERIOD_MS);
 	}
 }
-
-
 
 /*==================[external functions definition]==========================*/
 void app_main(void){
